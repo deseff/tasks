95e160bfd95288ec9e574e883c8be6243bb22768
deseff <blazej.kurjata@gmail.com>
2020-01-23 19:33:47 +0100

    Module 22.2 - solution of the task

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -31,21 +30,21 @@ public class TrelloClient {
     @Autowired
     private RestTemplate restTemplate;
 
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        URI url = getTrelloBoardsUri();
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        TrelloBoardDto[] notEmpty = Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]);
+        return Arrays.asList(notEmpty);
+    }
+
     private URI getTrelloBoardsUri() {
         return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUserName + "/boards") //
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("fields", "name,id")
                 .build().encode().toUri();
-    }
-
-    URI url = getTrelloBoardsUri();
-
-    public Optional<List<TrelloBoardDto>> getTrelloBoards() {
-
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
-
-        return Arrays.asList(Optional.ofNullable(boardsResponse).orElseGet(new TrelloBoardDto[]));
     }
 
     /*public List<TrelloBoardDto> getTrelloBoards() {
