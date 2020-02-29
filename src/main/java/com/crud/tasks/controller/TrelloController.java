a2b84e6427e619ef1e6174b905d35864e8381219
deseff <blazej.kurjata@gmail.com>
2020-02-29 20:02:14 +0100

    attempts to fix merging conflicts

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -17,25 +17,6 @@ public class TrelloController {
     @Autowired
     private TrelloClient trelloClient;
 
-/*    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-            System.out.println("This board contains lists: ");
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-        });
-
-
-        trelloBoards.stream()
-                .filter(trelloBoardDto -> !trelloBoardDto.getId().isEmpty())
-                .filter(trelloBoardDto -> !trelloBoardDto.getName().isEmpty())
-                .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-    }*/
-
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloClient.getTrelloBoards();
